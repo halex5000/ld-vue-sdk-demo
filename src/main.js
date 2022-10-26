f09ec93fdbb1afd0844310c98ba9b9742bf76897
Alexander Hardman <alexander.hardman@gmail.com>
2022-10-26 14:54:00 -0400

    updates to get all the vuetify stuff in

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
+import "vuetify/styles"; // Global CSS has to be imported
 import { createApp } from "vue";
-import "./style.css";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+}); // Replaces new Vuetify()
+
+app.use(vuetify);
+app.mount("#app");
