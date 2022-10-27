5bf33df2c06a169ef8f11b1a9fadb0014c2383cf
Alexander Hardman <alexander.hardman@gmail.com>
2022-10-27 15:38:27 -0400

    updating styling, bundling, and adding timeline

---
+++ src/components/Time-Line.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from "vue";
+
+let items = ref([
+  {
+    color: "#A34FDE",
+    icon: "mdi-rocket-launch",
+    title: "Initialize LaunchDarkly client",
+    description: "Setup the LaunchDarkly client with an SDK key",
+  },
+]);
+
+const loading = true;
+</script>
+
+<template>
+  <v-timeline align="start">
+    <v-timeline-item
+      v-for="(item, i) in items"
+      :key="i"
+      :dot-color="item.color"
+      :icon="item.icon"
+      fill-dot
+    >
+      <v-card
+        max-width="40%"
+        min-width="30%"
+        elevated
+        rounded="true"
+        :loading="loading"
+      >
+        <v-progress-linear
+          v-if="loading"
+          class="position-absolute"
+          style="z-index: 1"
+          color="#A34FDE"
+          height="10"
+          indeterminate
+        />
+        <v-card-title class="white text--primary text-h5">
+          {{ item.title }}
+        </v-card-title>
+        <v-card-text class="white text--primary">
+          <p>{{ item.description }}</p>
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+import "vuetify/styles"; // Global CSS has to be imported
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
 import { createApp } from "vue";
-import "./style.css";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+app.use(vuetify);
+app.mount("#app");
